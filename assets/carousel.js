f42ac23590683d048f836a60f88461d19f46bce1
luoyuqian0122 <165235405+luoyuqian0122@users.noreply.github.com>
2024-05-30 20:56:20 +0800

    edit11

--- assets/carousel.js
+++ assets/carousel.js
@@ -1,21 +1,5 @@
-let currentIndex = 0;
-const images = document.querySelectorAll('.carousel-images img');
-const totalImages = images.length;
+function showSlides(num) {
+    let slides = document.getElementsByClassName('slide');
+    
 
-function showNextImage() {
-    currentIndex++;
-    if (currentIndex >= totalImages) {
-        currentIndex = 0;
-    }
-    updateCarousel();
 }
-
-function updateCarousel() {
-    const carouselWidth = document.querySelector('.carousel').offsetWidth;
-    const newTransform = -currentIndex * carouselWidth;
-    document.querySelector('.carousel-images').style.transform = `translateX(${newTransform}px)`;
-}
-
-setInterval(showNextImage, 3000);
-
-window.addEventListener('resize', updateCarousel);
